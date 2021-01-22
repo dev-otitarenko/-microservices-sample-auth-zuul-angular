6d6190cc674106c8f607c178c8a4fc0acce98691
oleksii titarenko <atitarenko.eng@gmail.com>
2021-01-22 19:10:57 +0100

    service2: added DiscoveryEurekaClient, eurekaClient

--- ms-service-2/src/main/java/com/maestro/app/sample1/ms/service2/Service2Application.java
+++ ms-service-2/src/main/java/com/maestro/app/sample1/ms/service2/Service2Application.java
@@ -2,14 +2,18 @@ package com.maestro.app.sample1.ms.service2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
 @SpringBootApplication
 @EnableOAuth2Client
 @EnableFeignClients
-public class Service1Application {
+@EnableDiscoveryClient
+@EnableEurekaClient
+public class Service2Application {
 	public static void main(String[] args) {
-		SpringApplication.run(Service1Application.class, args);
+		SpringApplication.run(Service2Application.class, args);
 	}
 }

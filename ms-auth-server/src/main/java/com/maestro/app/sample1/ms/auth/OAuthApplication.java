f12190c3fe807f9f1f7f0e0fdd1fc67526f50efa
oleksii titarenko <atitarenko.eng@gmail.com>
2021-01-22 19:09:48 +0100

    auth-sever: added discoveryeureka

--- ms-auth-server/src/main/java/com/maestro/app/sample1/ms/auth/OAuthApplication.java
+++ ms-auth-server/src/main/java/com/maestro/app/sample1/ms/auth/OAuthApplication.java
@@ -2,10 +2,14 @@ package com.maestro.app.sample1.ms.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.context.request.RequestContextListener;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableEurekaClient
 public class OAuthApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OAuthApplication.class, args);
